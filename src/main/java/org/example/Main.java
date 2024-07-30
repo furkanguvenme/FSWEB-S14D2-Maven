9e5c46a9bc0b5f9d0d1053777d5076e0e035f499
furkanguvenme <furkanguven.me@gmail.com>
2024-07-30 13:36:58 +0300

    Exam Added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Bedroom bedroom = new Bedroom("Yatak Odası", new Wall("Sağ"), new Wall("Sol"), new Wall("Ön"), new Wall("Arka"), new Ceiling(20, PaintColor.WHITE), new Bed("Üç Kişilik", 5, 5 ,5 ,5),new Lamp(LampType.LAVA, true, 95),new Wardrobe(2 , 3, 50), new Carpet(2 , 3, PaintColor.GREEN));
         System.out.println(bedroom);
+        System.out.println("----------------------------");
     }
 }
